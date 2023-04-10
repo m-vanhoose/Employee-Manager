e0bcfc2c0b0cf1057ed67a3536e73f42e20d750c
m-vanhoose <m-vanhoose@live.com>
2023-04-10 17:00:45 -0400

    added functionality to api.js

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -6,12 +6,40 @@ const fs = require('fs');
 router.get('/notes', (req, res) => {
     const data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
     res.send(JSON.parse(data))
+});
+
+router.get('/notes/:title', (req, res) => {
+    const title = req.params.title;
+    readFromFile('../db/db.json')
+        .then((data) => JSON.parse(data))
 })
 
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newTitle = {
+            title,
+            text
+        };
+
+        readAndAppend(newTitle, '../db/db.json');
+        const response = {
+            status: 'success',
+            body: newTitle,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error')
+    }
+
+    console.log(req.body);
+});
 //readfile to get the current data
 //parse old data to normal js object
 //get new object from req.body
-//push new oibject into old array
+//push new object into old array
 //fs.writefile to create new db.json file
 //send new json array
 module.exports = router;
