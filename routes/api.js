047fc600cf5df38c53990bce0a18c171bbe63837
m-vanhoose <m-vanhoose@live.com>
2023-05-13 14:52:34 -0400

    fixed post route, added delete route

--- routes/api.js
+++ routes/api.js
@@ -2,15 +2,23 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
+const uniqid = require('uniqid');
+
+// const {
+//     handleNoteView,
+//     HandleNewNoteView,
+// } = require('../public/assets/js/index');
 
 router.get('/notes', (req, res) => {
     const data = fs.readFileSync(path.join(__dirname, '../db/db.json'));
-    res.send(JSON.parse(data))
+    res.send(JSON.parse(data));
+    // handleNoteView()
 });
 
 router.get('/notes/:title', (req, res) => {
+    console.log('notes/')
     const title = req.params.title;
-    readFromFile('../db/db.json')
+    fs.readFile(path.join(__dirname, '../db/db.json'))
         .then((data) => JSON.parse(data))
 })
 
@@ -20,9 +28,11 @@ router.post('/notes', (req, res) => {
     if (title && text) {
         const newTitle = {
             title,
-            text
+            text,
+            id: uniqid()
         };
         fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            console.log(data)
             if (err) {
                 console.error(err);
                 return;
@@ -33,20 +43,34 @@ router.post('/notes', (req, res) => {
                     if (err) {
                         console.error(err);
                     }
+                    res.json(newTitle)
                 })
             }
             
         })
-
-        // res.json(response);
     }
-
     console.log(req.body);
 });
-//readfile to get the current data
-//parse old data to normal js object
-//get new object from req.body
-//push new object into old array
-//fs.writefile to create new db.json file
-//send new json array
+
+router.delete('/notes/:id', (req, res) => {
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+            console.log(data)
+            if (err) {
+                console.error(err);
+                return;
+            } else {
+                const parsedData = JSON.parse(data);
+                const filteredNotes = parsedData.filter(note => note.id !== req.params.id)
+                fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(filteredNotes), err => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    res.json(filteredNotes)
+                })
+            }
+            
+        })
+    console.log(req.body);
+});
+
 module.exports = router;
